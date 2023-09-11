aeb4417576480256fb92ba08a7d5a42c91d52b85
OngakuKun <marcellehmkuhl@outlook.de>
2023-09-11 20:17:06 +0200

    Update Bot VERSION

--- src/defines.h
+++ src/defines.h
@@ -5,7 +5,7 @@
 // VERSIONING
 //----------------------------------------------------------------------------------
 #define NAME               "Nachtibot"
-#define VERSION            "0.0.0"
+#define VERSION            "0.0.1"
 #define RELEASE_DATE       "09/2023"
 
 #endif
